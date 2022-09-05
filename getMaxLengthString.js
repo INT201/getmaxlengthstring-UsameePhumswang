4ff4bc6237db932f0c4d0e0b026da5acf97119b1
UsameePhumswang <88129589+UsameePhumswang@users.noreply.github.com>
2022-09-05 21:31:39 +0700

    test1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,17 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  let longestWord = ''
+  for(let i = 0; i < arrayOfString.length; i++){
+    let currentWord = arrayOfString[i]
+  if(arrayOfString == null || arrayOfString == undefined){
+    return undefined
+  }else if(currentWord.length > longestWord.length){
+      longestWord = arrayOfString[i]
+    }
+  }
+  return longestWord
 }
 module.exports = getMaxLengthString
+
+console.log(getMaxLengthString(['hi','hello']))
